d0a61d25742427db2bbcc489221388958203ea91
pip-pipo <codemi692@gmail>
2020-12-25 10:31:56 -1100

    :books: Readme&bootstrap&routes added

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact'
+function App() {
+  return (
+    <div className="App">
+     <h2 className="">hello world</h2>
+     <Home />
+     <About />
+     <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+    return (
+        <div className="contact">
+            <h3>contact</h3>
+        </div>
+    )
+}
+
+export default contact
